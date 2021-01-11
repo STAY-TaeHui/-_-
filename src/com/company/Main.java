14f703c103f4af57b4bb16516139a389e90d8b78
SeoTaeHui <masta95@naver.com>
2021-01-11 16:52:43 +0900

    sucess

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bf.readLine());
+        int b = Integer.parseInt(bf.readLine());
+        long answer;
+        Solution s = new Solution();
+        answer = s.solution(a,b);
+        System.out.println(answer);
+    }
+}
+class Solution{
+    public long solution(int a, int b) throws IOException {
+        int tmp=0;
+
+        if(a==b){
+            return a;
+        }
+        else if(a>b){
+            for(int i=a; a>=b; a--){
+                tmp=a+tmp;
+            }
+            return tmp;
+        }
+        else
+            for(int i=a; a<=b; a++){
+                tmp = a + tmp;
+            }
+            return tmp;
     }
 }
