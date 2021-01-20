a70efda677a94c89e93e76fc5fb6291ac670818e
SeoTaeHui <masta95@naver.com>
2021-01-20 18:57:21 +0900

    같은 숫자는 싫어(time over)

--- 같은 숫자는 싫어/src/com/company/Main.java
+++ 같은 숫자는 싫어/src/com/company/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int[] arr = {1,1,3,3,0,1,1};
-        int[] arr2 = {4,4,4,3,3,};
+        int[] arr2 = {4,4,4,3,3};
         int[] arr3 = {1, 1, 2, 3, 2, 3, 4};
         Solution S = new Solution();
-        S.solution(arr);
+        S.solution(arr3);
     }
 }
 class Solution{
@@ -19,16 +19,18 @@ class Solution{
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i]!=arr[j]){
                     i=j;
+                    count++;
                     break;
                 }
+                else if(j== (arr.length-1) && i== (arr.length-2)){
+                    i++;
+                }
             }
-            count++;
         }
-        System.out.println("COUNT : "+count);
 
         int[] answer=new int [count];
 
-        for(int i=0,index=0; i<=arr.length-1&&index<count; index++){
+        for(int i=0,index=0; index<count; index++){
             answer[index]=arr[i];
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i]!=arr[j]){
@@ -37,10 +39,9 @@ class Solution{
                 }
             }
         }
-
-        for(int i: answer){
-            System.out.println(i);
-        }
+//        for(int i:answer){
+//            System.out.print(i+" ");
+//        }
         return answer;
     }
 }
