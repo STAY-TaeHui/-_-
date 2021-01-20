0e297e4eaa8d34ebb32fddd2b530f72b2a4069fb
SeoTaeHui <masta95@naver.com>
2021-01-20 17:05:35 +0900

    같은 숫자는 싫어 - Double_for

--- 같은 숫자는 싫어/src/com/company/Main.java
+++ 같은 숫자는 싫어/src/com/company/Main.java
@@ -6,20 +6,34 @@ public class Main {
 	// write your code here
         int[] arr = {1,1,3,3,0,1,1};
         int[] arr2 = {4,4,4,3,3,};
+        int[] arr3 = {1, 1, 2, 3, 2, 3, 4};
         Solution S = new Solution();
         S.solution(arr);
     }
 }
 class Solution{
     public int[] solution (int[] arr){
-        int[] answer=new int [arr.length];
-        int ans_len=0;
-        int tmp=0;
+        int count=1;
 
-        for(int i=0; i<arr.length-1; i++){
-            for(int j=1; j<arr.length; j++){
-                if(arr[i]==arr[j]){
+        for(int i=0; i<arr.length-1;){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]!=arr[j]){
+                    i=j;
+                    break;
+                }
+            }
+            count++;
+        }
+        System.out.println("COUNT : "+count);
 
+        int[] answer=new int [count];
+
+        for(int i=0,index=0; i<=arr.length-1&&index<count; index++){
+            answer[index]=arr[i];
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]!=arr[j]){
+                    i=j;
+                    break;
                 }
             }
         }
