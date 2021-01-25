06ddf9e01b2e144f65bf79c35d20d7b5776e4bff
SeoTaeHui <masta95@naver.com>
2021-01-25 20:14:40 +0900

    두 개 뽑아서 더하기

--- 두 개 뽑아서 더하기/src/com/company/Main.java
+++ 두 개 뽑아서 더하기/src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
@@ -15,15 +15,30 @@ public class Main {
 }
 class Solution{
     public int[] solution(int[] numbers){
-        int[] answer={};
-        ArrayList array = new ArrayList();
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
 
         for(int i=0; i<numbers.length-1; i++){
-            for(int j=i; j< numbers.length; j++){
-                array.add(numbers[i]+numbers[j]);
+            for(int j=i+1; j< numbers.length; j++){
+                if(!array.contains(numbers[i]+numbers[j])){
+                    array.add(numbers[i]+numbers[j]);
+                }
             }
         }
-        System.out.print(array);
+        Collections.sort(array);
+
+        int [] answer = new int[array.size()];
+
+        for(int i=0; i<answer.length; i++){
+            answer[i] = array.get(i);
+        }
+           /* //결과값 확인
+        System.out.print(array);    //중복제거 전
+
+        for(int i: answer){
+            System.out.print(i + " ");// 정렬 후
+        }
+*/
         return answer;
     }
 }
