4dd50bcd0f4bf8db7a2c562d7060898491e730bb
SeoTaeHui <masta95@naver.com>
2021-01-25 20:30:01 +0900

    두 개 뽑아서 더하기 + 주석

--- 두 개 뽑아서 더하기/src/com/company/Main.java
+++ 두 개 뽑아서 더하기/src/com/company/Main.java
@@ -16,21 +16,21 @@ public class Main {
 class Solution{
     public int[] solution(int[] numbers){
 
-        ArrayList<Integer> array = new ArrayList<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer>();    //Integer타입의 ArrayList생성
 
         for(int i=0; i<numbers.length-1; i++){
             for(int j=i+1; j< numbers.length; j++){
-                if(!array.contains(numbers[i]+numbers[j])){
-                    array.add(numbers[i]+numbers[j]);
-                }
+                if(!array.contains(numbers[i]+numbers[j])){     //numbers[]를 돌면서 바로다음 값과 더해주는데
+                    array.add(numbers[i]+numbers[j]);           //만약 array.contains()를 사용하여 더한 수가 array에 존재하지 않으면
+                }                                               //array.add()로 그 수를 넣어준다.
             }
         }
-        Collections.sort(array);
+        Collections.sort(array);                                //array 정렬
 
-        int [] answer = new int[array.size()];
+        int [] answer = new int[array.size()];                  //int배열을 반환하기위해 array.size크기만한 배열 생성
 
         for(int i=0; i<answer.length; i++){
-            answer[i] = array.get(i);
+            answer[i] = array.get(i);                           //List array값들을 answer배열에 복사해 준다.
         }
            /* //결과값 확인
         System.out.print(array);    //중복제거 전
